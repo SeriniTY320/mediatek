d00e27b7348b7a35750b79bb42ecd37f1702b360
rock12 <s2013srock@gmail.com>
2015-07-21 19:12:24 +0300

    Initial Commit

---
+++ custom/bird72_wet_a_jb3/cgen/inc/custom_cfg_module_file.h
@@ -0,0 +1,4 @@
+#include "mediatek/custom/bird72_wet_a_jb3/cgen/cfgfileinc/CFG_Custom1_File.h"
+#include "mediatek/custom/bird72_wet_a_jb3/cgen/cfgfileinc/CFG_GPS_File.h"
+#include "mediatek/custom/bird72_wet_a_jb3/cgen/cfgfileinc/CFG_PRODUCT_INFO_File.h"
+#include "mediatek/custom/bird72_wet_a_jb3/cgen/cfgfileinc/CFG_Wifi_File.h"
